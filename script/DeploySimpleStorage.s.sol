cc6ce7f053761ec01e130da6ba4cd1a23480d540
terrancrypt <thaiphamngoctuong@gmail.com>
2024-08-19 09:19:18 +0700

    update deploy script

--- script/DeploySimpleStorage.s.sol
+++ script/DeploySimpleStorage.s.sol
@@ -7,10 +7,9 @@ import {SimpleStorage} from "../src/SimpleStorage.sol";
 contract DeploySimpleStorage is Script {
     function run() external returns (SimpleStorage) {
         vm.startBroadcast();
-
         SimpleStorage simpleStorage = new SimpleStorage();
-
         vm.stopBroadcast();
+
         return simpleStorage;
     }
 }

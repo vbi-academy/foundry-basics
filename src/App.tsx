4082cbe74b19383bb5efcb9396a58cd21160fe78
terrancrypt <thaiphamngoctuong@gmail.com>
2024-08-20 09:29:15 +0700

    temporary update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import Button from "./components/Button";
+import Card from "./components/Card";
+import MainLayout from "./layouts/MainLayout";
+
 function App() {
-  return <>Hello world</>;
+  return (
+    <MainLayout>
+      <div className="flex gap-4">
+        <Card>
+          <p className="text-sm">Donation Information.</p>
+          <p>Address:</p>
+          <p>Total Amount Funded:</p>
+          <p>Total Funders:</p>
+        </Card>
+        <Card>
+          <p className="text-sm">Contribute your Ether.</p>
+          <input placeholder="Amount" className="p-2 border rounded-lg" />
+          <Button className="w-fit">Fund</Button>
+        </Card>
+      </div>
+    </MainLayout>
+  );
 }
 
 export default App;
